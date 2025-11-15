a91532b2dfad8643af768cdf26c56ccb2b5e1f51
Anggi Danu Saputra <danusaputra068@gmail.com>
2025-11-15 07:49:05 +0700

    feat: Add API routes for all endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\CartController;
+use App\Http\Controllers\API\OrderController;
+use App\Http\Controllers\API\PaymentController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Products
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{slug}', [ProductController::class, 'show']);
+
+// Cart
+Route::get('/cart', [CartController::class, 'index']);
+Route::post('/cart', [CartController::class, 'store']);
+Route::put('/cart/{id}', [CartController::class, 'update']);
+Route::delete('/cart/{id}', [CartController::class, 'destroy']);
+
+// Orders
+Route::post('/orders', [OrderController::class, 'store']);
+Route::get('/orders/{orderNumber}', [OrderController::class, 'show']);
+
+// Payment
+Route::post('/payment/notification', [PaymentController::class, 'notification']);
